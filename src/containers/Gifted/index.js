286a5cab15878e5d0080cc0f4e066f4085421c59
Moisés Maldaner de Lima <moiseslimax@gmail.com>
2020-06-10 13:58:01 -0300

    adding hocks to form

--- src/containers/Gifted/index.js
+++ src/containers/Gifted/index.js
@@ -1,27 +1,56 @@
 import React, { useState } from 'react';
-import { Button, FormField, Form, TextInput, Box, Select } from 'grommet';
+import { Button, FormField, Form, TextInput, Box, Select, RangeInput } from 'grommet';
 
 //styled components
 import { Container, BigText } from './styled.gifted';
 
 function Gifted() {
   const options = ['Feminino', 'Masculino', 'lgbt+', 'Isso realmente importa?'];
-  const [value, setValue] = useState({});
+  const [age, setAge] = useState(50);
+  const [name, setName] = useState('');
+  const [gender, setGender] = useState('');
+
+  function submitGiftedData() {
+    let gifted = {
+      name,
+      age,
+      gender,
+    };
+
+    //TODO Dispatch gifted
+
+    console.log(gifted);
+  }
+
   return (
     <Container>
       <BigText>
         Primeiramente, para quem vamos
         <br /> dar esse presente especial?
       </BigText>
-      <Form style={{ margin: 45 }} onSubmit={({ value }) => {}}>
+      <Form style={{ margin: 45 }} onSubmit={() => submitGiftedData()}>
         <FormField name="name" label="Name">
-          <TextInput id="text-input-id" name="name" />
+          <TextInput value={name} onChange={(e) => setName(e.target.value)} />
         </FormField>
         <FormField name="age" label="Idade">
-          <TextInput id="text-input-id" name="name" />
+          {age}
+          <RangeInput
+            min="0"
+            max="100"
+            step="1"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+          />
         </FormField>
         <FormField name="gender" label="Genero">
-          <Select id="select" name="select" placeholder="Selecione um genero" options={options} />
+          <Select
+            id="select"
+            name="select"
+            placeholder="Selecione um genero"
+            options={options}
+            value={gender}
+            onChange={({ option }) => setGender(option)}
+          />
         </FormField>
         <Box direction="row" style={{ marginTop: 30 }} gap="medium">
           <Button type="submit" primary label="Proximo passo!" />
